9a7447b6b768b787678d068c7a6de6142fda4406
gabemontero <gmontero@redhat.com>
2021-06-09 11:33:42 -0400

    bump(*) add tkn

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/shipwright-io/cli
 go 1.15
 
 require (
+	github.com/jonboulle/clockwork v0.1.1-0.20190114141812-62fb9bc030d1
 	github.com/onsi/gomega v1.10.3
 	github.com/pkg/errors v0.9.1
 	github.com/shipwright-io/build v0.4.0
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/pflag v1.0.5
+	github.com/tektoncd/cli v0.18.0
+	github.com/tektoncd/pipeline v0.23.0
 	github.com/texttheater/golang-levenshtein/levenshtein v0.0.0-20200805054039-cae8b0eaed6c
 	k8s.io/api v0.19.7
 	k8s.io/apimachinery v0.19.7
---
+++ vendor/github.com/tektoncd/cli/pkg/pods/fake/stream.go
@@ -0,0 +1,61 @@
+// Copyright © 2019 The Tekton Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fake
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"strings"
+
+	"github.com/tektoncd/cli/pkg/pods/stream"
+	corev1 "k8s.io/api/core/v1"
+	typedv1 "k8s.io/client-go/kubernetes/typed/core/v1"
+)
+
+type PodStream struct {
+	logs []Log
+	pods typedv1.PodInterface
+	name string
+	opts *corev1.PodLogOptions
+}
+
+func (ps *PodStream) Stream() (io.ReadCloser, error) {
+	for _, fl := range ps.logs {
+		if fl.PodName != ps.name {
+			continue
+		}
+
+		for _, c := range fl.Containers {
+			if c.Name == ps.opts.Container {
+				log := strings.Join(c.Logs, "\n")
+				return ioutil.NopCloser(strings.NewReader(log)), nil
+			}
+		}
+	}
+
+	return nil, fmt.Errorf("failed to stream container logs")
+}
+
+func Streamer(l []Log) stream.NewStreamerFunc {
+	return func(pods typedv1.PodInterface, name string, opts *corev1.PodLogOptions) stream.Streamer {
+		return &PodStream{
+			logs: l,
+			pods: pods,
+			name: name,
+			opts: opts,
+		}
+	}
+}
