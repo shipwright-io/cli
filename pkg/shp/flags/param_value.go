b4d535790933affe6e79bc3d4e61db4038ece61a
apoorvajagtap <apoorvajagtap4@gmail.com>
2024-07-31 18:12:24 +0530

    Adds flag to add paramValues
    This commit enables the option to pass custom/required parameters
    via CLI while creating the build or buildRun.

---
+++ pkg/shp/flags/param_value.go
@@ -0,0 +1,50 @@
+package flags
+
+import (
+	"fmt"
+
+	buildv1alpha1 "github.com/shipwright-io/build/pkg/apis/build/v1alpha1"
+)
+
+// ParamArrayValue implements pflag.Value interface, in order to store ParamValue key-value
+// pairs used on Shipwright's BuildSpec.
+type ParamArrayValue struct {
+	params *[]buildv1alpha1.ParamValue // pointer to the slice of ParamValue
+}
+
+// String prints out the string representation of the slice of EnvVar objects.
+func (p *ParamArrayValue) String() string {
+	slice := []string{}
+	for _, e := range *p.params {
+		slice = append(slice, fmt.Sprintf("%s=%v", e.Name, e.Value))
+	}
+	csv, _ := writeAsCSV(slice)
+	return fmt.Sprintf("[%s]", csv)
+}
+
+// Set receives a key-value entry separated by equal sign ("=").
+func (p *ParamArrayValue) Set(value string) error {
+	k, v, err := splitKeyValue(value)
+	if err != nil {
+		return err
+	}
+	for _, e := range *p.params {
+		if k == e.Name {
+			return fmt.Errorf("environment variable '%s' is already set", k)
+		}
+	}
+	*p.params = append(*p.params, buildv1alpha1.ParamValue{Name: k, SingleValue: &buildv1alpha1.SingleValue{Value: &v}})
+	return nil
+}
+
+// Type analogous to the pflag "stringArray" type, where each flag entry will be tranlated to a
+// single array (slice) entry, therefore the comma (",") is accepted as part of the value, as any
+// other special character.
+func (p *ParamArrayValue) Type() string {
+	return "stringArray"
+}
+
+// NewCoreEnvVarArrayValue instantiate a ParamValSliceValue sharing the EnvVar pointer.
+func NewParamArrayValue(params *[]buildv1alpha1.ParamValue) *ParamArrayValue {
+	return &ParamArrayValue{params: params}
+}
